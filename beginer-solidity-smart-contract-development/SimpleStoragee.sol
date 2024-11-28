eddbcb8f3a48110b2be92aca9e68c247efdd5457
unknown <rianziwalker@gmail.com>
2024-11-28 23:05:29 +0700

    added some functions

--- beginer-solidity-smart-contract-development/SimpleStoragee.sol
+++ beginer-solidity-smart-contract-development/SimpleStoragee.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.24;
 
 contract SimpleStorage {
-    
-}
+
+    uint256 private myFavoriteNumber;
+
+    struct Seseorang {
+        uint256 umur;
+        string name;
+    }
+
+    Seseorang[] public listOfWarga;
+
+    // Seseorang public tedy = Seseorang({favoriteNumber: 88, name: "tedy"});
+
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addSeseorang(string memory _name, uint256 _umur) public {
+        listOfWarga.push( Seseorang(_umur, _name) );
+    }
+ }
