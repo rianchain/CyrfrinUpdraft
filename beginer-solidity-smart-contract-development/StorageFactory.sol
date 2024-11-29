b222128543282d270f35c49cc2456b9c38fd50b6
unknown <rianziwalker@gmail.com>
2024-11-29 16:30:00 +0700

    -

--- beginer-solidity-smart-contract-development/StorageFactory.sol
+++ beginer-solidity-smart-contract-development/StorageFactory.sol
@@ -1,6 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+contract SimpleStorage {
+
+    uint256 private myFavoriteNumber;
+
+    struct Seseorang {
+        uint256 umur;
+        string name;
+    }
+
+    // dynamic arrays variable untuk menyimpan banyak data tak terhingga
+    Seseorang[] public listOfWarga;
+
+    // mapping untuk memetakan string ke angka (memasukan nama = hasilnya umur berupa angka)
+    mapping(string => uint256) public namaToUmur;
+
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addSeseorang(string memory _name, uint256 _umur) public {
+        listOfWarga.push( Seseorang(_umur, _name) );
+        namaToUmur[_name] = _umur;
+    }
+ }
+
 contract StorageFactory {
 
     
