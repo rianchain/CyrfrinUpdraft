013df1be591119c263a30abec8dfd0b6a323f01d
unknown <rianziwalker@gmail.com>
2024-11-29 19:25:44 +0700

    Add some function and inheritence

--- beginer-solidity-smart-contract-development/AddFiveStorage.sol
+++ beginer-solidity-smart-contract-development/AddFiveStorage.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
 
-contract AddFiveStorage {
+contract AddFiveStorage is SimpleStorage {
 
-    
+    function sayNigga() public pure returns(string memory) {
+        return "FUck Nigga!";
+    }
 
 }
