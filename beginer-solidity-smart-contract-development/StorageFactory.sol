29396ef0fc6b05bd3c1f7fe433ab82c0257edb28
unknown <rianziwalker@gmail.com>
2024-11-29 16:56:06 +0700

    add a new comment

--- beginer-solidity-smart-contract-development/StorageFactory.sol
+++ beginer-solidity-smart-contract-development/StorageFactory.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// hanya mengimport satu contract yaitu SimpleStorage saja di SimpleStorage.sol
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
-
+    // deklarasi contract yang kita import dengan penamaan baru
     SimpleStorage public simpleStorage;
 
 
