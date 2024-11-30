24959e6434344a36b01690197ee3ffdf7f60f173
unknown <rianziwalker@gmail.com>
2024-11-30 10:18:10 +0700

    -

--- beginer-solidity-smart-contract-development/AddFiveStorage.sol
+++ beginer-solidity-smart-contract-development/AddFiveStorage.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
 
+// inheritence contract
 contract AddFiveStorage is SimpleStorage {
 
     // fungsi overide untuk terhbung dan mengambil fungsi turunan dari SimpleStorage untuk di modifikasi
