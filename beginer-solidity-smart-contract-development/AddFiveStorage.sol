48801526e3a73a76db3bbd8523713c83d1bacaf9
unknown <rianziwalker@gmail.com>
2024-11-30 10:19:46 +0700

    -

--- beginer-solidity-smart-contract-development/AddFiveStorage.sol
+++ beginer-solidity-smart-contract-development/AddFiveStorage.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// importing other contract for inherintence and modify function 
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 // inheritence contract
