bb07d8df94be55071c8f882d5e28e5bf70352441
etherian <rianziwalker@gmail.com>
2024-12-03 20:29:18 +0700

    Importing chainlink

--- beginer-solidity-smart-contract-development/FundMe/FundMe.sol
+++ beginer-solidity-smart-contract-development/FundMe/FundMe.sol
@@ -6,6 +6,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     uint256 public minimumUsd = 10;
