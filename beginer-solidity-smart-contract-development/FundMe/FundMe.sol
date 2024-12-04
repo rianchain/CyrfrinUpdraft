b1e3037e3d1b03c4aed99f8f179ad0ab18ec4ea8
etherian <rianziwalker@gmail.com>
2024-12-04 16:40:19 +0700

    added a new variable mapping address to uint

--- beginer-solidity-smart-contract-development/FundMe/FundMe.sol
+++ beginer-solidity-smart-contract-development/FundMe/FundMe.sol
@@ -12,12 +12,14 @@ contract FundMe {
 
     uint256 public minimumUsd = 5000000000000000;
     address public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
 
     function fund() public payable {
         // Allow users to send $
         // Have a minimum sent in $
         require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH. At least Minimum deposit is 1 ETH!"); // 1e18 = 1 eth = 100000000000000000
+        funders.push(msg.sender);
     }
 
     function withdraw() public {
