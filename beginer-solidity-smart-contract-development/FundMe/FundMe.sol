70e82161dec3ab4545bc9da63adc765ee53ff1b6
etherian <rianziwalker@gmail.com>
2024-12-04 17:04:23 +0700

    fill the PriceConverter solidity file by copy paste logic from fundMe

--- beginer-solidity-smart-contract-development/FundMe/FundMe.sol
+++ beginer-solidity-smart-contract-development/FundMe/FundMe.sol
@@ -6,7 +6,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import Price
 
 contract FundMe {
 
@@ -27,29 +27,6 @@ contract FundMe {
 
     }
 
-    function getPrice() public view returns (uint256) {
-        // Address of ETH/USD : 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (
-        , // uint80 roundID
-        int256 price,
-        , // uint256 startedAt
-        , // uint256 updatedAt
-        // uint80 answeredInRound
-    ) = priceFeed.latestRoundData();
-    return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        // msg.value.getConversionRate();
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
+    
 
 }
--- beginer-solidity-smart-contract-development/FundMe/PriceConverter.sol
+++ beginer-solidity-smart-contract-development/FundMe/PriceConverter.sol
@@ -1,3 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter {
+
+
+    function getPrice() internal  view returns (uint256) {
+        // Address of ETH/USD : 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (
+        , // uint80 roundID
+        int256 price,
+        , // uint256 startedAt
+        , // uint256 updatedAt
+        // uint80 answeredInRound
+    ) = priceFeed.latestRoundData();
+    return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
+        // msg.value.getConversionRate();
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
+}
+
