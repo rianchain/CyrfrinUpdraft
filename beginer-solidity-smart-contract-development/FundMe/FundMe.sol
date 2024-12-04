5f915621a6127e23b5c1856f8cb9453b74b5cb4d
etherian <rianziwalker@gmail.com>
2024-12-04 21:50:44 +0700

    -

--- beginer-solidity-smart-contract-development/FundMe/FundMe.sol
+++ beginer-solidity-smart-contract-development/FundMe/FundMe.sol
@@ -11,7 +11,8 @@ import {PriceConverter} from "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5000000000000000;
+    uint256 public minimumUsd = 5e18;
+
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
@@ -19,13 +20,18 @@ contract FundMe {
     function fund() public payable {
         // Allow users to send $
         // Have a minimum sent in $
+        // msg.value.getConversionRate();
+        // uint256 amountInUsd = msg.value.getConversionRate();
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH. At least Minimum deposit is 1 ETH!"); // 1e18 = 1 eth = 100000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     function withdraw() public {
-
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
     }
 
     
