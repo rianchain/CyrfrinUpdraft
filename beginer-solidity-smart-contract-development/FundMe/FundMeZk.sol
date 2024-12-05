3eacff1ca7ce1ceecc6ef771bcdf5f35e4d6ef11
etherian <rianziwalker@gmail.com>
2024-12-05 15:08:20 +0700

    Updating FundMe and PriceConverter Zksync solidity version

--- beginer-solidity-smart-contract-development/FundMe/FundMeZk.sol
+++ beginer-solidity-smart-contract-development/FundMe/FundMeZk.sol
@@ -9,7 +9,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {PriceConverter} from "./PriceConverterZk.sol";
+library PriceConverter {
+
+
+    function getPrice() internal  view returns (uint256) {
+        // Address of ETH/USD ZKSYNC : 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        (
+        , // uint80 roundID
+        int256 price,
+        , // uint256 startedAt
+        , // uint256 updatedAt
+        // uint80 answeredInRound
+    ) = priceFeed.latestRoundData();
+    return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
+        // msg.value.getConversionRate();
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF).version();
+    }
+
+}
+
 
 error NotOwner();
 
