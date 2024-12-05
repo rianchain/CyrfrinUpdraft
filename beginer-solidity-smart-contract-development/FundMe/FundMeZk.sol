5625b80a851d5dfef0654e93372039f9a7c7b5d6
etherian <rianziwalker@gmail.com>
2024-12-05 15:06:48 +0700

    Updating FundMe and PriceConverter Zksync solidity version

--- beginer-solidity-smart-contract-development/FundMe/FundMeZk.sol
+++ beginer-solidity-smart-contract-development/FundMe/FundMeZk.sol
@@ -7,7 +7,7 @@
 // Set a minimum funding value in usd
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.24;
 
 import {PriceConverter} from "./PriceConverterZk.sol";
 
